1b69c16d62cb518a6d9e8170905884e7481cee60
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-09-21 04:01:01 -0300

    Código mais bonitinho

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import openai
 
 from secret import api_OpenAI
 openai.api_key = api_OpenAI
-print('\n\n -------------------------- Programa que gera o que você pedir --------------------------\n')
+print('\n\n -------------------------- Programa que gera o que você pedir --------------------------\n\nPara sair é só digitar: --sair\n')
 
-while True:
+def teste_saida(entrada):
+  if entrada.lower() == '--sair':
+    exit(0)
 
-  entrada = input('Humano: ')
-
+def pergunta_ai(entrada):
   try:
     response = openai.Completion.create(
       model="text-davinci-002",
-      prompt = entrada + 'em Português',
+      prompt=entrada + '. responda em Português',
       temperature=0.5,
       max_tokens=500,
       top_p=1.0,
       frequency_penalty=0.0,
       presence_penalty=0.0
     )
-    print('\nIA: '+response['choices'][0]['text'].strip()+'\n')
+    print('\nIA: ' + response['choices'][0]['text'].strip() + '\n')
   except Exception:
     print('Não deu certo')
+
+
+while True:
+  entrada: str = input('Humano: ')
+  teste_saida(entrada)
+  pergunta_ai(entrada)
+
